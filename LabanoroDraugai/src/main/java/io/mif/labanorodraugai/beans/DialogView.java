6325e56a4121c44743e69556d3bca29a24529948
MindaugasJokubonis <mindaugas.jokubonis@gmail.com>
2016-05-26 12:51:37 +0300

    Fix membership

--- LabanoroDraugai/src/main/java/io/mif/labanorodraugai/beans/DialogView.java
+++ LabanoroDraugai/src/main/java/io/mif/labanorodraugai/beans/DialogView.java
@@ -21,7 +21,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
- 
+
 import org.primefaces.event.CloseEvent;
 
 /**
@@ -31,26 +31,31 @@ import org.primefaces.event.CloseEvent;
 @Named
 @ManagedBean
 public class DialogView {
+
     @PersistenceContext
     EntityManager em;
-    
+
     @Inject
     private AuthenticationBean authenticationBean;
 
     @Inject
     private AccountController accountController;
-    
+
     public void extendMembership() {
         //addMessage("System Error", "Please try again later.");
-        Account currentAccount = authenticationBean.getLoggedAccount();
-        
+        int accId = authenticationBean.getLoggedAccount().getId();
+        Account currentAccount = em.createNamedQuery("Account.findById", Account.class)
+                .setParameter("id", accId)
+                .getSingleResult();
+        //Account currentAccount = authenticationBean.getLoggedAccount();
+
         //EntityManager em = ejbFacade.getEntityManager();
-        Config config = em.createNamedQuery("Config.getConfig", Config.class).getSingleResult();        
+        Config config = em.createNamedQuery("Config.getConfig", Config.class).getSingleResult();
         int price = config.getMembershipPrice();
         int seasonDays = config.getSeasonDays();
-        
+
         // if account has not enough points return error
-        if (currentAccount.pointsQuantity.intValue()<price) {
+        if (currentAccount.pointsQuantity.intValue() < price) {
             JsfUtil.addErrorMessage("Klaida: Neužtenka taškų narystei pratęsti.");
             return;
         } //todo error message
@@ -62,15 +67,15 @@ public class DialogView {
         } //todo error message
 
         //if new member
-        if(currentAccount.membershipValidUntil==null){
+        if (currentAccount.membershipValidUntil == null) {
             currentAccount.membershipValidUntil = new Date(new java.util.Date().getTime());
-        }; 
+        };
 
         GregorianCalendar newDate = new GregorianCalendar();
         //currentAccount.membershipValidUntil.getYear(), currentAccount.membershipValidUntil.getMonth(), currentAccount.membershipValidUntil.getDay()
         newDate.setTime(new Date(new java.util.Date().getTime()));
         // Add days
-        currentAccount.membershipValidUntil = new java.sql.Date(newDate.getTimeInMillis());        
+        currentAccount.membershipValidUntil = new java.sql.Date(newDate.getTimeInMillis());
         newDate.add((GregorianCalendar.DATE), seasonDays);
         currentAccount.membershipValidUntil = new java.sql.Date(newDate.getTimeInMillis());
 
@@ -78,22 +83,22 @@ public class DialogView {
         currentAccount.pointsQuantity = currentAccount.pointsQuantity.subtract(new BigDecimal(price));
         accountController.setSelected(currentAccount);
         accountController.update();
-        
+
         return;
     }
-     
+
     public void addMessage(String summary, String detail) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
-    
+
     public String getFormMessage() {
         Account currentAccount = authenticationBean.getLoggedAccount();
-        Config config = em.createNamedQuery("Config.getConfig", Config.class) .getSingleResult();
-        String msg = config.getSeasonDays() + " dienų pratęsimas kainuoja " + config.getMembershipPrice() + " taškų (turimi taškai: "+ currentAccount.getPointsQuantity() +"). Ar norite tęsti?";
+        Config config = em.createNamedQuery("Config.getConfig", Config.class).getSingleResult();
+        String msg = config.getSeasonDays() + " dienų pratęsimas kainuoja " + config.getMembershipPrice() + " taškų (turimi taškai: " + currentAccount.getPointsQuantity() + "). Ar norite tęsti?";
         return msg;
     }
-    
+
     public boolean isValid() {
         Account currentAccount = authenticationBean.getLoggedAccount();
         return currentAccount.isValid();
